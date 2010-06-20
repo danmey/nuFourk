39604935f857bf134224ea7844f84aea0da1e8fb
Wojciech Daniel Meyer <wojciech.meyer@gmail.com>
2010-06-20 01:15:43 +0100

    * engine.ml: tide up data structures. 	start working on implementation.

--- src/engine.ml
+++ src/engine.ml
@@ -29,26 +29,87 @@ module Cell = struct
   type t = Value.t
 end
 
-module Word = struct
-  type kind = Immediate | Executed
-  type t = { name:Name.t; kind:kind; code:Opcode.t list }
+(*
+module Stackop = struct
+  let push = Stack.push
+  let pop = Stack.pop
+  let swap stack = 
+    let a = pop stack in
+    let b = pop stack in
+      push a stack; push b stack
+  let rot stack =
+    let a = pop stack in
+    let b = pop stack in
+    let c = pop stack in
+      push b stack; push c stack; push stack a
 end
+*)
 
-module Dictionary = struct
-  type entry = Word.t list
-  type t = (Name.t, entry) Hashtbl.t
+module Stack = struct
+  type 'a t = 'a list
+  let push value stack = value::stack
+  let pop (value::stack) = value,stack
+  let create() = []
+end
+module rec Word : sig type t end = struct
+  type kind = Immediate | Compiled
+  type code = Core of (Model.t -> Model.t) | User of Opcode.t list
+  type t = { name:Name.t; code:code; kind:kind; }
+  let call word = word
+end
+and Dictionary : sig 
+  type t 
+  val lookup : t -> Name.t -> Word.t
+	 val add    : t -> Name.t -> Word.t -> unit 
+	 val create : unit -> t 
+end = struct
+  type t = (Name.t, Word.t) Hashtbl.t
   let create() = Hashtbl.create 1000
+  open Hashtbl
+  let lookup = find  
+  let add = add	
 end
-
-module Model = struct
-  type t = { istack:int Stack.t; fstack:float Stack.t; cells:Cell.t array; dict:Dictionary.t }
+and Model : sig 
+  type t = 
+      { istack:int Stack.t; 
+	fstack:float Stack.t; 
+	cells:Cell.t array; 
+	dict:Dictionary.t; 
+	state:State.t } 
+  val pushi : t -> int -> t
+  val pushf : t -> float -> t
+  val lookup : t -> Name.t -> Word.t
+end = struct
+  type t = 
+      { istack:int Stack.t; 
+	fstack:float Stack.t; 
+	cells:Cell.t array; 
+	dict:Dictionary.t; 
+	state:State.t }
   let create heap_size = { 
     istack = Stack.create(); 
     fstack = Stack.create(); 
     cells  = Array.create heap_size Value.Empty; 
-    dict   = Dictionary.create() 
+    dict   = Dictionary.create();
+    state  = State.Interpreting;
   }
+  open Stack
+  let pushi model v = { model with istack=push v model.istack }
+  let pushf model v = { model with fstack=push v model.fstack }
+  let lookup model = Dictionary.lookup model.dict
 end
 
-module Interpret = struct
+module Run = struct
+  open Parser
+  open State
+  open Model
+  let run model token = 
+    match model.state with
+      | Interpreting -> 
+	(match token with
+	  | Token.Integer value -> pushi model value
+	  | Token.Float value -> pushf model value
+	  | Token.Word name -> 
+	    let word = lookup model name in
+	      model)
 end
