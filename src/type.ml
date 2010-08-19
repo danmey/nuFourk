a839a2b15775fd36ccd39178043845d3b59cf482
Wojciech Meyer <wojciech.meyer@gmail.com>
2010-08-19 19:01:12 +0100

    * make.sh: Remove temporarily type.ml.   * type.ml: More typechecking.

--- src/type.ml
+++ src/type.ml
@@ -10,22 +10,66 @@ type signature = {
     
 type annotation = opcode * signature
 
+type func = string * code
+
+type dictionary = func list
+
 let basic_type name = U.Term ( name, [ ] )
 
 let t_int = basic_type "int"
+
 let t_float = basic_type "float"
 
-(*
-let annotate { 
+let closure_type { 
   input = input; 
   output = output;
 } = 
+  U.Term ( "closure", 
+	   U.Term ( "input", input ), 
+	   U.Term ( "output", output ) )
+
+exception Type_error of string
+
+let type_error signature signature' =
+  let str = 
+    Printf.sprintf "Expected type `%s', found `%s'!" 
+      (U.to_string signature) 
+      (U.to_string signature')
+  in
+  raise (Type_error str)
+
+let rec check dictionary ( { 
+  input = input; 
+  output = output;
+} as current ) =
   function
-    | PushInt of int ->
-      
-    | PushFloat of float -> 
-    | Call of string ->
-    | PushCode of code ->
-    | App ->
+    | PushInt _     -> { current with input = t_int :: input }
+    | PushFloat _   -> { current with input = t_float :: input }
+    | Call name     -> check dictionary current (assoc dictionary name)
+    | PushCode code -> { current with input = closure_type (check dictionary current code) :: input }
+    | App           -> 
+      match input with
+	| func :: input -> 
+	  begin
+	    let check
+	    match func with
+	      | U.Term ( "closure", 
+			 U.Term ( "input", input' ), 
+			 U.Term ( "output", output' ) ) ->
+		if input <> input' then type_error input input
+		else { current with input = output  input }
+
+(*
+let rec basic_type_to_string a_type =
+  function
+    | U.Term ( name, [ ] ) -> name
+    | _ -> failwith "basic_type_to_string"
+and arguments_of_string a_types =
+  String.concat 
+and type_to_string
+    
+    function
+    | U.Term ( "closure", 
+	       U.Term ( "input", input ), 
+	       U.Term ( "output", output ) ) -> Printf.printf "closure of type ( %s )" (type_of_string 
 *)
-  
