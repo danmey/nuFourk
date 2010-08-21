f6286ae8bb550d12b92f0318181746ee30343d56
Wojciech Meyer <wojciech.meyer@gmail.com>
2010-08-21 18:43:35 +0100

    pending changes

--- src/type.ml
+++ src/type.ml
@@ -1,4 +1,4 @@
-(*---------------------------------------------------------------------------- 
+(*----------------------------------------------------------------------------
 nuFourk - Concatenative, stack based, functional, strictly typed language.
 Copyright (C) 2010 Wojciech Meyer
 
@@ -18,64 +18,86 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 open Unify
 
-open BatList
+(* open BatList *)
 
 type signature = {
   input  : U.t list;
   output : U.t list;
 }
-    
+
+let void_signature = {
+  input = [];
+  output = [];
+}
+
 type annotation = opcode * signature
 
-type func = string * code
+type func = string * signature
 
 type dictionary = func list
 
 let basic_type name = U.Term ( name, [ ] )
 
-
 let t_int = basic_type "int"
 
 let t_float = basic_type "float"
 
-let closure_type { 
-  input = input; 
+let closure_type {
+  input = input;
   output = output;
-} = 
-  U.Term ( "closure", 
-	   U.Term ( "input", input ), 
-	   U.Term ( "output", output ) )
+} =
+  U.Term ( "closure",
+	   [
+	     U.Term ( "input", input ),
+	     U.Term ( "output", output )
+	   ])
 
 exception Type_error of string
 
 let type_error signature signature' =
-  let str = 
-    Printf.sprintf "Expected type `%s', found `%s'!" 
-      (U.to_string signature) 
+  let str =
+    Printf.sprintf "Expected type `%s', found `%s'!"
+      (U.to_string signature)
       (U.to_string signature')
   in
   raise (Type_error str)
 
-let rec check dictionary ( { 
-  input = input; 
+let rec check dictionary ( {
+  input = input;
   output = output;
 } as current ) =
+
+  let rec apply_func =
+    function
+      | input, [] -> input
+      | [], x :: xs -> x :: apply_func ([], xs)
+      | x :: xs, y :: ys ->
+	if x <> y then type_error [x] [y]
+	else x :: apply_func (xs, ys)
+  in
+
   function
-    | PushInt _     -> { current with input = t_int :: input }
-    | PushFloat _   -> { current with input = t_float :: input }
-    | Call name     -> check dictionary current (assoc dictionary name)
-    | PushCode code -> { current with input = closure_type (check dictionary current code) :: input }
-    | App           -> 
-      match input with
-	| func :: input -> 
+    | PushInt _     -> { current with output = t_int :: output }
+    | PushFloat _   -> { current with output = t_float :: output }
+    | PushCode code -> { current with output = closure_type (check dictionary current code) :: output }
+    | Call name     ->
+      let { input = input'; output = output' } = assoc dictionary name
+      in
+	{ input = apply_func (input', output);
+	  output = output' }
+    | App ->
+      match output with
+	| func :: output ->
 	  begin
 	    let check
 	    match func with
-	      | U.Term ( "closure", 
-			 U.Term ( "input", input' ), 
+	      | U.Term ( "closure",
+			 U.Term ( "input", input' ),
 			 U.Term ( "output", output' ) ) ->
-		if input <> input' then type_error input input
-		else { current with input = output  input }
+		if output <> output' then type_error output output'
+		else { current with output = output  input }
+
+
 
 (*
 let rec basic_type_to_string a_type =
@@ -83,11 +105,11 @@ let rec basic_type_to_string a_type =
     | U.Term ( name, [ ] ) -> name
     | _ -> failwith "basic_type_to_string"
 and arguments_of_string a_types =
-  String.concat 
+  String.concat
 and type_to_string
-    
+
     function
-    | U.Term ( "closure", 
-	       U.Term ( "input", input ), 
-	       U.Term ( "output", output ) ) -> Printf.printf "closure of type ( %s )" (type_of_string 
+    | U.Term ( "closure",
+	       U.Term ( "input", input ),
+	       U.Term ( "output", output ) ) -> Printf.printf "closure of type ( %s )" (type_of_string
 *)
